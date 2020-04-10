d997a55a3a3a7cfc59584d04352eb15c0dd53328
Wil Goolsby <wil@wilgoolsby.com>
2020-04-10 08:27:27 -0400

    Added files and setup to start generating grid squares

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('#main');
+
+// Create the grid square
+const square = document.createElement('div');
+square.classList.add('grid-square');
+
+// Make 40 of them per row and column
+
+container.appendChild(square);
