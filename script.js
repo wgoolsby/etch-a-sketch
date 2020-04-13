8eafdec73890f524bc5bf034ef88a2de9f2201ed
Wil Goolsby <wil@wilgoolsby.com>
2020-04-12 21:00:25 -0400

    make the squares change color when the mouse enters

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const container = document.querySelector('#main');
 
-// Create the grid square
-const square = document.createElement('div');
-square.classList.add('grid-square');
+const makeGrid = (count = 20) => {
+  const gridSize = 640;
+  const squareSize = gridSize / count;
+  for (i = 0; i < count * count; i++) {
+    createSquare(i, squareSize);
+  }
+};
 
-// Make 40 of them per row and column
+const createSquare = (id, size) => {
+  let divNumber = id;
+  let entries = 0;
+  // Create the grid square
+  divNumber = document.createElement('div');
+  // Size it based on what makeGrid() said to size it to
+  divNumber.style.height = size + 'px';
+  divNumber.style.width = size + 'px';
+  // Stick it on the #main div
+  container.appendChild(divNumber);
+  // Create an event listener to update the color every time the mouse comes in
+  divNumber.addEventListener('mouseenter', () => {
+    entries += 0.1;
+    divNumber.style.backgroundColor = 'rgba(0, 0, 0, ' + entries + ')';
+  });
+};
 
-container.appendChild(square);
+const resetCanvas = () => {
+  console.log('resetting');
+  container.innerHTML = '';
+  userSize = window.prompt('How many squares per side?');
+  makeGrid(parseInt(userSize));
+};
+
+makeGrid();
